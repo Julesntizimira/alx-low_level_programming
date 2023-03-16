0e694870383864cbbe0462276c3c2ba5541ce631
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-16 14:17:33 -0700

    sdhjd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-char v[100] = {"\"Programming is like building a multilingual puzzle\n"};
+char v[52] = {"\"Programming is like building a multilingual puzzle\n"};
 
 puts(v);
 
