d7aea3897930c5a0169fe536db03c9fadaf123d5
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-16 16:16:35 -0700

    shjGVDXSA

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 /**
- *Main entry point
+ *main - entry point
  *
- * return Always 1  (failure)
+ * return 1 (failure)
  */
 int	main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return 1;
+return (1);
 }
 
