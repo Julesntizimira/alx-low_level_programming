2d66c0e7d96a29c8fba741af0a60d63cd11f1a5a
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-17 15:46:23 -0700

    sjdhbcd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Starting point
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+/*Declaration*/
+int i;
+for (i = 122; i >= 97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
