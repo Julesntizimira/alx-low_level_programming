a4c9c9029897030524b722940062801b98b21a71
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-17 15:57:34 -0700

    adhbbd

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,14 +8,14 @@ int main(void)
 {
 /*Declaration*/
 int i;
-for(i = 0; i <= 9; i++)
+for (i = 0; i <= 9; i++)
 {
 putchar(i + '0');
 }
-for(i = 10; i <= 15; i++)
+for (i = 10; i <= 15; i++)
 {
 putchar(i + 'a' - 10);
 }
-putchar('\n'); 
+putchar('\n');
 return (0);
 }
