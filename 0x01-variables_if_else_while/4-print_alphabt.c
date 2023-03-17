fd458cc487fc85428b68a5eacd2f8388469a4f39
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-17 15:07:24 -0700

    ajkhsbxchkads

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Starting point
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+/*Declaration*/
+int i; 
+for (i = 97; i <= 122; i++)
+{
+if (i == 101 )
+continue;
+else if (i == 113 )
+continue;
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
