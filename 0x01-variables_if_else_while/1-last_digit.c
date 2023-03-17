408ab61a0c0a244b38497d56e0a3bf8c85a8c234
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-17 13:31:32 -0700

    akhcbd

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ n = rand() - RAND_MAX / 2;
 /* conditional if statement */
 if (n > 5)
 {
-printf("Last digit of n is %i and is greater than 5 \n", n);
+printf("Last digit of %i is %i and is greater than 5 \n", n, n);
 }
 else    if (n == 0)
 {
-printf("Last digit of n is %i and is 0", n);
+printf("Last digit of %i is %i and is 0", n, n);
 }
 else	if ((n <= 6) || (n != 0))
 {
-printf("Last digit of n is %i and is less than 6 and not 0\n", n);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n);
 }
 return (0);
 }
