65103488512b55829205e219ed3d59dc6480d6c7
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-17 14:55:26 -0700

    adhbcsdbc

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Starting point
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+/*Declaration*/
+int i;
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+for (i = 65; i <= 90; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
