9278538ca583adae5c5a1588aa978fc928e30c06
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-17 14:20:41 -0700

    ehdbbh

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %i is %i and is greater than 5\n", n, l);
 }
 else    if (l == 0)
 {
-printf("Last digit of %i is %i and is 0", n, l);
+printf("Last digit of %i is %i and is 0\n", n, l);
 }
 else    if ((l <= 6) || (n != 0))
 {
