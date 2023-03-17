1862370b180d9a197eb61916baf10a486bec9bed
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-17 15:55:44 -0700

    adhbbd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Starting point
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+/*Declaration*/
+int i;
+for(i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+for(i = 10; i <= 15; i++)
+{
+putchar(i + 'a' - 10);
+}
+putchar('\n'); 
+return (0);
+}
