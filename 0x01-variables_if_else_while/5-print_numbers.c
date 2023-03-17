52ec7f0a2cbbf63ce31291975acbedf009dd959c
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-17 15:17:50 -0700

    ajkhsbxchkads

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Starting point
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+/*Declaration*/
+int i;
+for ( i = 0; i < 10; i++)
+{
+printf ("%i \n", i);
+}
+return(0);
+}
