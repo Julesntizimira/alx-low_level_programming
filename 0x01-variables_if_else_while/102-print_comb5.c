cfdfdc5c8ffb48b875894ef2aa5398814d747b96
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-17 18:42:08 -0700

    dashbd

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,19 +7,19 @@
 int main()
 {
 int i, j;
-for(i=0; i<=99; i++)
+for (i=0; i<=99; i++)
 {
-for(j=i; j<=99; j++)
+for (j=i; j<=99; j++)
 {
 int num1 = i / 10;
 int num2 = i % 10;
 int num3 = j / 10;
 int num4 = j % 10;
-if(i == j)
+if (i == j)
 {
 continue;
 }
-if(i > j)
+if (i > j)
 {
 continue;
 }
@@ -29,12 +29,13 @@ putchar(' ');
 putchar(num3 + '0');
 putchar(num4 + '0'); 
 
-if(i != 98 || j != 99)
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
 }
+putchar('\n');
 return (0);
 }
