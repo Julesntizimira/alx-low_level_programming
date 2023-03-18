637e74804671ea83fbd986dc77f4a821479b0d3d
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-17 17:10:13 -0700

    khdjbaah

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,15 @@ int main(void)
 int i, j;
 for (i = 0; i <= 9; i++)
 {
-for (j = 0; j <= 9; i++)
+for (j = 0; j <= 9; j++)
 {
-putchar(i + '0');
-putchar(j + '0');
 if (i > j)
 continue;
-else if ((i == 8) && (j == 9))
+if (i == j)
+continue;
+putchar(i + '0');
+putchar(j + '0');
+if ((i == 8) && (j == 9))
 break;
 putchar(44);
 putchar(' ');
