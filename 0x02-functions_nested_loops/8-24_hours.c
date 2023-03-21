359013d1f7e6d3fe592e1033034c769480944a51
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 15:28:37 -0700

    akshjcks

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,17 +16,15 @@ void jack_bauer(void)
 			{
 				 for (z = 0; z < 10; z++)
 				 {
-					 if ((i == 2) && (j == 4) && (k == 0) && (z == 0))
+					 while (((i != 2) && (j != 4)) && ((k != 0) && (z != 0)))
                                          {
-                                                 break;
-                                         }
 					 _putchar(i + '0');
 					 _putchar(j + '0');
 					 _putchar(':');
 					 _putchar(k + '0');
 					 _putchar(z + '0');
 					 _putchar('\n');
-					 
+					 }
 				 }
 			}
 		}
