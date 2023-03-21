4a5cbac036a01464b64d6b64a1ddef405b58b3e3
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 07:01:32 -0700

    ajshbcab

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * a function that prints the alphabet, in lowercas
+ */
+void print_alphabet(void)
+{
+	for (i = 97; i <=122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
