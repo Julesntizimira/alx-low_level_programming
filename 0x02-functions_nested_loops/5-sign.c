bd69759171f40424b1e341e193c92a07153ab791
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 14:02:58 -0700

    akshjcks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,23 +9,23 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchhar('+');
-		_putchhar(',');
-		_putchhar(' ');
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchhar('0');
-		_putchhar(',');
-		_putchhar(' ');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		return (0);
 	}
 	else
 	{
-		_putchhar('-');
-		_putchhar(',');
-		_putchhar(' ');
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
 		return(-1);
 	}
 }
