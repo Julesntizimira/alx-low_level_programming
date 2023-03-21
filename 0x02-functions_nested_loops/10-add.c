dc44474d38b7d2ed380a09e8d17ba7d219405375
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 16:16:48 -0700

    kajhsba

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - checks for lowercase
+ * @i: is an argument of type int
+ * @j: is an argument of type int
+ * Return: returns the sum of i,j.
+ */
+
+int add(int i, int j)
+{
+	int z;
+	z = i + j;
+	return (z);
+}
