4dbf6de3869ecb0b77e8dfc60a6efd04ab33e6d0
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 07:49:36 -0700

    ajshbcab

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _islower - checks for lowercase
+ *
+ * Return: 1 if c is lowercase and 0 otherwise
+ */
+
+int _islower(int c)
+{
+        if (putchar('c') >= 97 || putchar('c') <= 122)
+	{
+		return 1;
+	}
+	else
+		return 0;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,6 @@ void print_alphabet(void);
 
 /*void print_alphabets ten times */
 void print_alphabet_x10(void);
+/* checks for lowercase */
+int _islower(int c);
+
