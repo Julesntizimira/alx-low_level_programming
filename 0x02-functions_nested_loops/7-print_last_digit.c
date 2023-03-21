4928df674eabd13b5046e650f6a21c2305a4ea38
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 12:15:00 -0700

    skjd

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_last_digit - checks for lowercase
+ * @i: is an argument of type int
+ * Return: return the remainder
+ */
+
+int print_last_digit(int i)
+{
+	int z;
+
+	z = i % 10;
+	return (z);
+}
