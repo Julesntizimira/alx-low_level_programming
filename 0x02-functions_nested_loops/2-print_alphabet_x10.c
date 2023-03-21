798258ae5350c00e83831715536d9387d10b381c
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 07:35:38 -0700

    ajshbcab

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,14 @@
 void print_alphabet_x10(void)
 {
 	int i, j;
+
 	j	=	0;
 
 	while (j < 10)
 	{
 		for (i = 97; i <= 122; i++)
 		{
-                putchar(i);
+			putchar(i);
 		}
 		putchar('\n');
 		j++;
