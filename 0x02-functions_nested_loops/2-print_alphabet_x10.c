20ab996c8417b319ac7079be2200ec3b1bfbba11
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 07:27:31 -0700

    ajshbcab

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Prints the alphabet
+ */
+
+void print_alphabet_x10(void);
+{
+	
+        int i, j;
+	j = 0;
+
+	while (j < 10)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+                putchar(i);
+		}
+		putchar('\n');
+		j++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,3 +3,6 @@ void pirint_putchar(void);
 
 /*prints lowercase alphabet*/
 void print_alphabet(void);
+
+/*void print_alphabets ten times */
+void print_alphabet_x10(void);
