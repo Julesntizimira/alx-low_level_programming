89968d48ba0bd2e54009545f9d9a27d0dcfc9955
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-22 14:46:19 -0700

    shdbks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,51 @@
 #include <stdio.h>
-
-void print_times_table(int n) {
-    int i, j;
-    if (n < 0 || n > 15) {
-        return;
-    }
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= n; j++) {
-            printf("%d\t", i*j);
-        }
-        printf("\n");
-    }
-}
-
+#include "main.h"
+/*
+ * print_times_table - printing table
+ */
+void print_times_table(int n)
+{
+	int i, j, z;
+	
+	if ((n >= 0) && (n <= 15))
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (j == 0)
+				z = n;
+			else if (j != 0)
+				z = n * j;
+			
+			for(i = 0; i <= z; i++)
+			{
+				if(j == 0)
+				{
+					printf("%d", j);
+					if (i != z)
+					{
+						putchar(',');
+						printf("   ");
+					}
+				}
+				else if(((i % j) == 0) && (j != 0))
+				{
+					if (i != 0)
+					{
+						if (i < 10)
+							printf("   "):
+						else if ((i >= 10) && (i < 100))
+							printf("  ");
+						else
+							printf(" ");
+					}
+					printf("%d", i);
+					if (i != z)
+					{
+						putchar(',');
+					}
+				}
+			}
+			putchar('\n');
+		}
+	 }
+}                                                                                                                                                                    52,1          Bot
