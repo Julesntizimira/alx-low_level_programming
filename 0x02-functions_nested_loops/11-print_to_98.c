7a76a54ef294eb22258502b7b58f331cefad5e22
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 18:32:46 -0700

    kajhsba

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,79 +3,37 @@
 /**
  * print_to_98 - checks for lowercase
  */
-
 void print_to_98(int n)
 {
-	int i, j, z, y;
-
-	if (n >= 0)
+	if (n <= 98)
 	{
-
-		z = n % 10;
-		y = n / 10;
-		for (i = y; i <= 9; i++)
-		{
-			for (j = 0; j < 10; j++)
+		for (; n <= 98; n++)
+		{ if (n == 98 )
 			{
-				if ((i = y) && (j < z))
-				{
-					continue;
-				}
-				if ((i = 9) && (j = 9))
-				{
-					break;
-				}
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(',');
-					_putchar(' ');
-				
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
 			}
 		}
 	}
-	if (n < 0)
+	else
 	{
-
-
-                n = n * -1;
-		z = n % 10;
-                y = n / 10;
-                for (i = y; i >= 0; i--)
-                {
-                        for (j = 9; j >= 0; j--)
-                        {
-                                if ((i = y) && (j > z))
-                                {
-					continue;
-				}
-				if ((i = 9) && (j = 9))
-                                {
-                                        break;
-                                }
-			        _putchar('-'); 
-				_putchar(i + '0');
-			       	_putchar(j + '0');
-			       	_putchar(',');
-			       	_putchar(' ');
-                                
-                        }
-                }
-		for ( i = 0; i < 10; i++)
+		for (; n >= 98; n--)
 		{
-			for ( j = 0; j < 10; j++)
+			if (n == 98)
 			{
-				if ((i = 9) && (j = 9))
-                                {
-                                        break;
-                                }
-				_putchar(i + '0');
-				_putchar(j + '0');
-			       	_putchar(',');
-			       	_putchar(' ');
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n)
 			}
 		}
 	}
-	
-
-
 }
