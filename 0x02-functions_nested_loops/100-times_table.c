fec492fecea706ceed801061121e5ce9b480943e
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 18:51:23 -0700

    kajhsba

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-
+#include "main.h"
 void print_times_table(int n) {
+    int i, j;
     if (n < 0 || n > 15) {
         return;
     }
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= n; j++) {
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= n; j++) {
             printf("%-3d ", i*j);
         }
         printf("\n");
     }
 }
+
