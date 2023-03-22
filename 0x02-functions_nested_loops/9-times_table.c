ec5b8da79b18dfc644cd6e723428a8756ed9f657
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-21 18:06:30 -0700

    kajhsba

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,96 +5,35 @@
 int _putchar(char c);
 void times_table(void)
 {
-	int i, j, k, z, n, m, v, w, y, b;
 
-	for ( i = 0; i < 10; i++)
-	{ 
-		putchar(48);
-		if (i == 9)
-			break;
-		putchar(44);
-		putchar(' ');
-	}
-	putchar('\n');
-	for (j = 48; j <= 57; j++)
+	int x, y, z, u, d;
+	for(x = 0; x <= 9;; x++)
 	{
-		putchar(i);
-                putchar(44);
-                putchar(' ');
+		for(y =0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
+		_putchar('\n');
+		}
 	}
-	putchar('\n');
-	for (k = 0; k < 18; k++)
-        {
-		if ( (k % 2) != 0)
-			continue;
-                _putchar(i + '0');
-                _putchar(44);
-                _putchar(' ');
-        }
-        putchar('\n');
-	for (z = 0; z <= 27; z++)
-        {
-                if ( (z % 3) != 0)
-                        continue;
-                putchar(i + '0');
-                putchar(44);
-                putchar(' ');
-        }
-	putchar('\n');
-	for (m = 0; m <= 36; m++)
-        {
-                if ( (z % 4) != 0)
-                        continue;
-                putchar(m + '0');
-                putchar(44);
-                putchar(' ');
-        }
-        putchar('\n');
-	for (n = 0; n <= 45; n++)
-        {
-                if ( (z % 5) != 0)
-                        continue;
-                putchar(i + '0');
-                putchar(44);
-                putchar(' ');
-        }
-        putchar('\n');
-	for (v = 0; v <= 54; v++)
-        {
-                if ( (v % 6) != 0)
-                        continue;
-                putchar(i + '0');
-                putchar(44);
-                putchar(' ');
-        }
-        putchar('\n');
-	for (w = 0; w <= 63; w++)
-        {
-                if ( (z % 7) != 0)
-                        continue;
-                putchar(i + '0');
-                putchar(44);
-                putchar(' ');
-        }
-        putchar('\n');
-	 for (y = 0; y <= 72; y++)
-        {
-                if ( (z % 8) != 0)
-                        continue;
-                putchar(i + '0');
-                putchar(44);
-                putchar(' ');
-        }
-        putchar('\n');
-	 for (b = 0; b <= 81; b++)
-        {
-                if ( (z % 9) != 0)
-                        continue;
-                putchar(i + '0');
-                putchar(44);
-                putchar(' ');
-        }
-        putchar('\n');
 
 }
 
