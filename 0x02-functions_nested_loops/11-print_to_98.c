dcc849a8fbdb61d1e8b5978f0d7286b2ee101150
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-22 10:44:48 -0700

    akjdncadk

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,36 +6,11 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
-	{
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
+        int i;
+
+        for (i = n; i <= 98; i++)
+        {
+                printf("%d, ", i);
+        }
+        printf("\n");
 }
