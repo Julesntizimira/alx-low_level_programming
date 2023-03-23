dbd282947be9e168ad85936ba1770cd0ba274ca1
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-22 19:17:12 -0700

    ajks

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,36 +7,33 @@
  * Return: always return 0 (success)
  */
 void fibona(void);
-
 int main(void)
 {
-        fibona();
-        return  (0);
+	fibona();
+	return  (0);
 }
 /**
  * fibona - a void function
  */
 void fibona(void)
 {
-        long unsigned z, k, j, w, y;
+	unsigned long z, k, y, j;
 
-        z = 1;
-        k = 2;
+	z = 1;
+	k = 2;
 	y = 0;
-        for (j = 1; j <= 50; j++)
-        {
-                printf("%lu", z);
-                printf(", ");
-		y = y + z;
-		if (y > 4000000)
-			break;
-                w = z + k;
-                z = k;
-                k = w;
-        }
-	putchar('\n');
-
+	for (j = 1; j <= 50; j++)
+	{
+		w = z + k;
+		if ((z % 2) == 0)
+		{
+			y = y + z;
+			if (y > 4000000)
+				break;
+		}
+		z = k;
+		k = w;
+	}
 	printf("%lu", y);
-
-        putchar('\n');
+	putchar('\n');
 }
