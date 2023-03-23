3100f8c408f543666af01558b4e0bf46447c8c47
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-23 04:15:36 -0700

    ahb

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,7 +13,7 @@ int largest_number(int a, int b, int c)
 
 	int largest;
 
-	if ((a > b) && (a > c))
+	if ((a > c) && (c > b))
 	{
 		largest = a;
 	}
@@ -21,11 +21,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if ((b > a) && (b > c))
+	else if ((b > a) && (a > c))
 	{
 		largest = b;
 	}
-	else if ((b > a) && (a > c))
+	else if ((b > c) && (c > a))
 	{
 		largest = b;
 	}
