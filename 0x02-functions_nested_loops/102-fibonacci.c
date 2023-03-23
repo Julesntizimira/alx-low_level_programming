1ccafc772cb1dec2eea969f7cbaac882c49f9ef1
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-22 17:31:55 -0700

    aksak

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - void function
+ *
+ * Return: always return 0 (success)
+ */
+void fib(void);
+
+int main (void)
+{
+	fib();
+	return(0);
+}
+void fib(void)
+{
+	int i, j, k, z, w;
+	z = 1;
+	k = 2;
+	for ( j = 1; j <= 50; j++)
+	{
+	
+		printf("%d", z);
+		if (j == 50)
+			break;
+		printf(", ");
+		w = z + k;
+		z = k;
+		k = w;
+	}
+	putchar('\n');
+}
