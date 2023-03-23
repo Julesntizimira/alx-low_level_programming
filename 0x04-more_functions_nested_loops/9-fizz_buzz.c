ff73a6a9b7112e0b288ab56ad2ea064e99c1168f
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-23 11:17:48 -0700

    as,jncxas,x

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - printing the upper
+ *
+ * Return: 0 always success
+ */
+
+void Fizz_Buzz(void);
+
+int main(void)
+{
+
+	Fizz_Buzz();
+	return (0);
+}
+
+/**
+ * Fizz_Buzz - printing the upper
+ */
+void Fizz_Buzz(void)
+{
+
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (((i % 3) == 0) && ((i % 5) != 0))
+		{
+			printf("Fizz");
+		}
+		else if (((i % 3) != 0) && ((i % 5) != 0))
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+}
