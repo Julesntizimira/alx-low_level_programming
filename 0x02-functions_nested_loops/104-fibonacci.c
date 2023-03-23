3e3b14217d533f20ca7cc5aaf343f77d8f607e0d
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-22 19:51:30 -0700

    ahsk

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,12 +18,12 @@ int main(void)
  */
 void fib(void)
 {
-       long z, k, j, w;
+       long unsigned z, k, j, w;
         z = 1;
         k = 2;
         for (j = 1; j <= 98; j++)
         {
-                printf("%lo", z);
+                printf("%lu", z);
                 if (j == 98)
                         break;
                 printf(", ");
