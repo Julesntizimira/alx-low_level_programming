3ed203b0414e79e165447e20aee6f858da371fa4
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-23 07:27:07 -0700

    akjsk

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - printing the uppe
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			_putchar(i + '0');
+		}
+		for (i = 0; i <= 4; i++)
+		{
+			_putchar(1 + '0');
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
