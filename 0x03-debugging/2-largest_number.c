43cc8b59754cfbc5d7d436939532e0ba31a82b99
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-23 04:01:28 -0700

    asjnl

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,10 +17,19 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
+	else if ((a > b) && (b > c))
+	{
+		largest = a;
+	}
+
 	else if ((b > a) && (b > c))
 	{
 		largest = b;
 	}
+	else if ((b > a) && (a > c))
+        {
+                largest = b;
+        }
 	else
 	{
 		largest = c;
