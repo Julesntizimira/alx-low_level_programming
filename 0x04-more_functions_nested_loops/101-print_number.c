73748fad4bcc9b0858a1005243a4e5ce3197d4a0
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-23 17:48:08 -0700

    kjsn

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_number - printing the upper
+ *
+ * @n: inputs
+ */
+
+
+void print_number(int n)
+{
+	int i, p;
+
+	i = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if ((n < 10) && (n >= 0))
+	{
+		_putchar(n + '0');
+	}
+	else if (n >= 10)
+	{
+		if ((n >= 10) && (n < 100))
+			p = 10;
+		else if ((n >= 100) && (n < 1000))
+			p = 100;
+		else if ((n >= 1000) && (n < 10000))
+			p = 1000;
+		else if (n >= 10000)
+			p = 10000;
+		while (p >= 10)
+		{
+			i = (n / p);
+			_putchar(i + '0');
+			n = (n % p);
+			p = p / 10;
+			if (p == 1)
+				_putchar(n + '0');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,5 +14,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void Fizz_Buzz(void);
-
+void print_number(int n);
 #endif
