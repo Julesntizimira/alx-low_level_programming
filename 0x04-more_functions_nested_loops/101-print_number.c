bca37052537dcd65bf4c3b9b168917b766ad9dab
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-23 18:11:59 -0700

    akjncda

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,21 @@
 #include "main.h"
-
 /**
  * print_number - printing the upper
  *
  * @n: inputs
  */
-
-
 void print_number(int n)
 {
 	int i, p;
 
 	i = 0;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
 	}
 	if ((n < 10) && (n >= 0))
-	{
 		_putchar(n + '0');
-	}
 	else if (n >= 10)
 	{
 		if ((n >= 10) && (n < 100))
@@ -30,8 +24,18 @@ void print_number(int n)
 			p = 100;
 		else if ((n >= 1000) && (n < 10000))
 			p = 1000;
-		else if (n >= 10000)
+		else if ((n >= 10000) && (n < 100000))
 			p = 10000;
+		else if ((n >= 100000) && (n < 1000000))
+			p = 100000;
+		else if ((n >= 1000000) && (n < 10000000))
+			p = 1000000;
+		else if ((n >= 10000000) && (n < 100000000))
+			p = 10000000;
+		else if ((n >= 100000000) && (n < 1000000000))
+			p = 100000000;
+		else if ((n >= 1000000000) && (n < 10000000000))
+			p = 1000000000;
 		while (p >= 10)
 		{
 			i = (n / p);
