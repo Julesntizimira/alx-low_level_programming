fc9566519cd3b9a1589074b2db0a476b9396eac7
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-23 18:27:21 -0700

    abhdahd

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,14 @@ void print_number(int n)
 	int i, p;
 
 	i = 0;
-	if (n < 0)
+	if ((n < 0) && (n >= -2147483648))
 	{
 		_putchar('-');
 		n = n * -1;
 	}
 	if ((n < 10) && (n >= 0))
 		_putchar(n + '0');
-	else if (n >= 10)
+	else if ((n >= 10) && (n <= 2147483647))
 	{
 		if ((n >= 10) && (n < 100))
 			p = 10;
