1f0f4a2571b7e0435be70b7dab1492326c1974f4
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-23 18:48:15 -0700

    ajhsbx

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,49 +1,23 @@
 #include "main.h"
+
 /**
  * print_number - printing the upper
  *
  * @n: inputs
  */
+
 void print_number(int n)
 {
-	int i, p;
+	unsigned int j = n;
 
-	i = 0;
 	if (n < 0)
 	{
+		n *= -1;
+		j = n;
 		_putchar('-');
-		n = n * -1;
 	}
-	if ((n < 10) && (n >= 0))
-		_putchar(n + '0');
-	else if ((n >= 10) && (n <= 2147483647))
-	{
-		if ((n >= 10) && (n < 100))
-			p = 10;
-		else if ((n >= 100) && (n < 1000))
-			p = 100;
-		else if ((n >= 1000) && (n < 10000))
-			p = 1000;
-		else if ((n >= 10000) && (n < 100000))
-			p = 10000;
-		else if ((n >= 100000) && (n < 1000000))
-			p = 100000;
-		else if ((n >= 1000000) && (n < 10000000))
-			p = 1000000;
-		else if ((n >= 10000000) && (n < 100000000))
-			p = 10000000;
-		else if ((n >= 100000000) && (n < 1000000000))
-			p = 100000000;
-		else if (n >= 1000000000)
-			p = 1000000000;
-		while (p >= 10)
-		{
-			i = (n / p);
-			_putchar(i + '0');
-			n = (n % p);
-			p = p / 10;
-			if (p == 1)
-				_putchar(n + '0');
-		}
-	}
+	j /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
 }
