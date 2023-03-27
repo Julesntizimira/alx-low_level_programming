82542dd36633550d9f07369b29331ee6e6b51152
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-27 13:41:59 -0700

    shbkh

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * print_array - print in reverse
+ *
+ * @a: str
+ * @n: str
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i == n - 1)
+			break;
+
+		printf(", ");
+
+	}
+	printf("\n");
+}
