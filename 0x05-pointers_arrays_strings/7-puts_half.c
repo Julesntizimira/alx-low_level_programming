066554d230b541b08029784f33c26363fdf5ca9a
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-27 13:06:02 -0700

    akjcba

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * puts_half - print in reverse
+ *
+ * @str: str
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	char *n = str;
+
+	while (*n != '\0')
+	{
+		n++;
+		i++;
+	}
+	i = i / 2;
+	str = str + i;
+	while (str < n)
+	{
+		char c;
+
+		c = *str;
+		_putchar(c);
+		str++;
+	}
+	_putchar('\n');
+}
