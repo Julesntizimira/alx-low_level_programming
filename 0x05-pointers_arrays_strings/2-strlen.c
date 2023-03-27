9e4816082390050a53907a1a4f96cc38947d22d8
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-27 07:18:49 -0700

    asjhbx

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * swap_int - swaping values
+ * @a: pointer
+ * @b: pointer
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	
+	i = sizeof(*s) / sizeof(s[0]);
+
+
+	return (i);
+
+
+}
