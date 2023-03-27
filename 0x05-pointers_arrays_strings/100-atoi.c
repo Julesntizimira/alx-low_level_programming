092d2957e4761915ad8374320e7ed3aa9978963b
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-27 14:42:06 -0700

    ,jsb,

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,41 +9,24 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    long result = 0;
+	int result = 0;
+	int sign = 1;
 
-    // define min and max values for an int
-    int int_max = ~(1 << (sizeof(int) * 8 - 1));
-    int int_min = 1 << (sizeof(int) * 8 - 1);
-
-    // skip whitespace
-    while (*s == ' ' || (*s >= '\t' && *s <= '\r')) {
-        s++;
-    }
-
-    // handle sign
-    if (*s == '-') {
-        sign = -1;
-        s++;
-    } else if (*s == '+') {
-        s++;
-    }
-
-    // process digits
-    while (*s >= '0' && *s <= '9') {
-        if (result > int_max / 10 || (result == int_max / 10 && (*s - '0') > int_max % 10)) {
-            // handle overflow
-            if (sign == 1) {
-                return int_max;
-            } else {
-                return int_min;
-            }
-        }
-        result = result * 10 + (*s - '0');
-        s++;
-    }
-
-    return (int)(sign * result);
-}
-
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			sign = -sign;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			result = result * 10 + (*s - '0');
+		}
+		else if (result > 0)
+		{
+			break;
+		}
+		s++;
+	}
+	return sign * result;
 }
