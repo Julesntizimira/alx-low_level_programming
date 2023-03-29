59e8210b111b83424a7cd6908d86df2d767cf292
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-29 13:45:35 -0700

    jwhda

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,21 +8,28 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	char *v = dest;
 	char *p = src;
-	int z, j;
+	int z, j, i;
 
 	z = 0;
-
 	while (*p != '\0')
 	{
 		p++;
 	}
 	j = p - src;
+	while (*v != '\0')
+	{
+		v++;
+	}
+	i = v - dest;
 
 	if (n > j)
 	{
 		while (z < n)
 		{
+			if (z > i)
+				break;
 			if (z < j)
 				*(dest + z) = *(src + z);
 			else
@@ -34,6 +41,8 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		while (z < n)
 		{
+			if (z > i)
+				break;
 			*(dest + z) = *(src + z);
 			z++;
 		}
