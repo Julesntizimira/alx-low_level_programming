061f645b16828df210e00e0e0abc89b8a8094a90
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-29 09:53:22 -0700

    adjkbdbk

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,29 @@
-  1 # include "main.h"
-  2 
-  3 char *_strncat(char *dest, char *src, int n)
-  4 {
-  5 
-  6 
-  7 
-  8    char *p = dest;
-  9    int z, j;
- 10 
- 11 
- 12 
- 13   while (*p != '\0')
- 14   {
- 15  p++;
- 16   }
- 17 
- 18   j = p - dest;
- 19 
- 20 
- 21 
- 22 
- 23   for (z = 0; z <  n; z++)
- 24   {
- 25   *(dest + j) = *(src + z);
- 26   j++;
- 27   }
- 28 
- 29   return (dest);
- 30 
- 31 
- 32 }
-~       
+# include "main.h"
+
+/**
+ * _strncat - function
+ *
+ * @dest: input
+ * @src: input
+ *
+ * @n: input
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+	int z, j;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	j = p - dest;
+	for (z = 0; z <  n; z++)
+	{
+		*(dest + j) = *(src + z);
+		j++;
+	}
+	return (dest);
+}
