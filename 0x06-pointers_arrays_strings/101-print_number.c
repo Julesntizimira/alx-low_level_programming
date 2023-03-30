364b5cbaa709ca83f679717ee6f51b06ae2149f0
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-30 07:58:52 -0700

    ajhsb

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,43 @@
+# include "main.h"
+
+/**
+ * print_number - print num
+ *
+ * @n: jenh
+ */
+
+void print_number(int n)
+{
+	unsigned int v;
+	int i, p;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	if (n < 10 && n >= 0)
+	{
+		_putchar(n + '0');
+	}
+	else if (n >= 10)
+	{
+		p = 1;
+		v = n;
+		while (v >= 10)
+		{
+			p = p * 10;
+			v = v / 10;
+		}
+		while (p >= 10)
+		{
+			i = n / p;
+			n %= p;
+			_putchar(i + '0');
+			p /= 10;
+			if (p < 10)
+				_putchar(n + '0');
+		}
+	}
+}
