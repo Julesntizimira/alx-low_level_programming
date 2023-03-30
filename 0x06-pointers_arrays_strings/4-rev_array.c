12487758241c77b5402f881421d421a85c4fb414
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-30 00:37:44 -0700

    sjdhjh

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, j;
+	int i, j, k;
 
-	if (n > 0)
+	if (n >= 0)
 	{
-		for (i = 0; i < n / 2; i++)
+		for (i = 0, k = n - 1; i < k; i++, k--)
 		{
 			j = a[i];
-			a[i] = a[n - 1 - i];
-			a[n - 1 - i] = j;
+			a[i] = a[k];
+			a[k] = j;
 			n--;
 		}
 	}
