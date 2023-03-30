b5182594f072e99f0b776ffc5228b2be3c47789e
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-30 03:15:38 -0700

    adcdf

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+# include "main.h"
+/**
+ * cap_string - function
+ * @s: input
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	char k;
+	int j, y;
+
+	y = 32;
+	for (j = 0; *(s + j) != '\0'; j++)
+	{
+		if (s[j] >= 97 && s[j] <= 122)
+		{
+			if (s[j - 1] == ' ' || s[j - 1] == '\t' ||
+			s[j - 1] == '.' || s[j - 1] == '\n' ||
+			s[j - 1] == '{' || s[j - 1] == ';' ||
+			s[j - 1] == ',' || s[j - 1] == '?' ||
+			s[j - 1] == ')' || s[j - 1] == '(' ||
+			s[j - 1] == '}' || s[j - 1] == ''' ||
+			s[j - 1] == '"' || s[j-1] == '!')
+			{
+				k = s[j] - y;
+				s[j] = k;
+			}
+		}
+	}
+	return (s);
+}
