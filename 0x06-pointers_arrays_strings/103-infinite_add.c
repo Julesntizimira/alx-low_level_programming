4d29a17b572ea1d013340d232e36f770836f9649
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-30 10:45:06 -0700

    ajhdb

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,57 +1,51 @@
 # include "main.h"
+/**
+ * infinite_add - func
+ * @n1: input
+ * @n2: input
+ * @r: in
+ * @size_r: in
+ * Return: res
+ */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
 {
-    int i, z, k, p, y = 0;
-    char *v = n1;
-    char *w = n2;
+	int len1 = 0, len2 = 0, i, max_len, carry = 0;
 
-    while (*v != '\0')
-    {
-        v++;
-    }
-    z = v - n1;
+	while (n1[len1])
+	{
+		len1++;
+	}
+	while (n2[len2])
+	{
+		len2++;
+	}
+	max_len = len1 > len2 ? len1 : len2;
+	if (max_len + 1 >= size_r)
+	{
+		return (0);
+	}
+	for (i = 1; i <= max_len; i++)
+	{
+		int digit1 = i <= len1 ? n1[len1 - i] - '0' : 0;
 
-    while (*w != '\0')
-    {
-        w++;
-    }
-    k = w - n2;
+		int digit2 = i <= len2 ? n2[len2 - i] - '0' : 0;
 
-    if ((z >= size_r) || (k >= size_r))
-        return (0);
+		int sum = digit1 + digit2 + carry;
 
-    k--;
-    z--;
-    for (i = size_r - 1; i >= 0; i--, k--, z--)
-    {
-        if ((z >= 0) && (k >= 0))
-        {
-            p = (n1[z] - '0') + (n2[k] - '0') + y; 
-            r[i] = (p % 10) + '0'; 
-            y = p / 10;
-        }
-        else if ((z < 0) && (k >= 0))
-        {
-            p = (n2[k] - '0') + y;
-            r[i] = (p % 10) + '0';
-            y = p / 10;
-        }
-        else if ((z >= 0) && (k < 0))
-        {
-            p = (n1[z] - '0') + y;
-            r[i] = (p % 10) + '0';
-            y = p / 10;
-        }
-        else
-        {
-            break;
-        }
-    }
+		carry = sum / 10;
 
-    if (y > 0) 
-        return (0);
-
-    return (r);
+		r[max_len - i + 1] = sum % 10 + '0';
+	}
+	if (carry > 0)
+	{
+		r[0] = carry + '0';
+		r[max_len + 1] = '\0';
+	}
+	else
+	{
+		r[max_len] = '\0';
+	}
+	return (r);
 }
-
