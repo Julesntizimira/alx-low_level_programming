76d79516ecdf7d469cfc150d5fbd299d4e90b793
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-29 23:24:45 -0700

    kjbkk

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+# include "main.h"
+
+/**
+ * reverse_array - function
+ * @a: input
+ * @n: input
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		j = *(a + i);
+		*(a + i) = *(a + n - 1 - i);
+		*(a + n - 1 - i) = j;
+		n--;
+	}
+}
