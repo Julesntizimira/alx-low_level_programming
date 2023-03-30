f3354e8226f18051dcccd2e730f096435d2a0501
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-30 00:02:29 -0700

    hdhd

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -24,7 +24,8 @@ void reverse_array(int *a, int n)
 	}
 	else if (n % 2 == 0)
 	{
-		for (i = 0; i < n / 2; i++)
+		n--;
+		for (i = 0; i <= n / 2; i++)
 		{
 			j = *(a + i);
 			*(a + i) = *(a + n - i);
