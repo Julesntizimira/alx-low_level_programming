2d7c7ce2338398cc300db0b1643b43b5edc19ace
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-03-30 18:08:29 -0700

    ahjkbdcd

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,50 +1,38 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - funct
- * @b: input
- * @size: size
+ * print_buffer - prints the content of size bytes of a buffer
+ * @b: the buffer to print
+ * @size: the number of bytes to print from the buffer
+ *
+ * Return: void
  */
-
 void print_buffer(char *b, int size)
 {
 	int i, j;
 
-	printf("0");
-	printf("        ");
-	for (i = 0; i < 10; i++)
-	{
-		printf("%02x ", i);
-	}
-	printf("\n");
 	for (i = 0; i < size; i += 10)
 	{
-		printf("%08x:", i);
+		printf("%08x: ", i);
 		for (j = i; j < i + 10; j++)
 		{
 			if (j < size)
-				printf(" %02x", b[j]);
+				printf("%02x", *(b + j));
 			else
-				printf("   ");
-		}
-		printf(" ");
-		for (j = i; j < i + 10; j++)
-		{
-			if (j < size)
-			{
-				if (b[j] < 32 || b[j] > 126)
-					printf(".");
-				else
-					printf("%c", b[j]);
-			}
-			else
+				printf("  ");
+			if (j % 2 != 0)
 				printf(" ");
 		}
-		printf("\n");
-	}
-	if (size <= 0)
-	{
+		for (j = i; j < i + 10; j++)
+		{
+			if (j >= size)
+				break;
+			if (*(b + j) >= 32 && *(b + j) <= 126)
+				printf("%c", *(b + j));
+			else
+				printf(".");
+		}
 		printf("\n");
 	}
 }
