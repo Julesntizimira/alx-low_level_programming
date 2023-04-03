47145eff688a7464fe56fe907881ebbc1a5caaed
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-03 03:32:22 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+# include "main.h"
+# include <stddef.h>
+/**
+ * _strchr - memset funct
+ * @s: input
+ * @c: input
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i, j;
+	char *n = s;
+
+	while (*n != '\0')
+	{
+		n++;
+		j++;
+	}
+	for (i = 0; i < j; i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
