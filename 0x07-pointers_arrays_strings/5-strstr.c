8b6828c5e059101da3ce59b59594b44ad0723845
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-03 09:44:41 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+# include "main.h"
+# include <stddef.h>
+/**
+ * *_strstr - memset funct
+ * @needle: input
+ * @haystack: input
+ * Return: i
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, z, j, k;
+
+	k = 0;
+	if (needle == NULL)
+	{
+		return (haystack);
+	}
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		z = k;
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+			{
+				k = k + 1;
+				break;
+			}
+		}
+		if (k != z)
+			return (needle + i);
+	}
+	return (NULL);
+}
