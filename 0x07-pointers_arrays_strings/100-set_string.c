60607d98aaa404ccc5f9ecc9c8a4d4096dd7442f
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-03 15:07:57 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * set_string - prints the sum of the elements on the main diagonal and
+ * the secondary diagonal of a square matrix of integers
+ *
+ * @s: pointer to a 2D array of integers representing the square matrix
+ * @to: integer representing the size of the square matrix
+ * Return: void.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
