79e6a19b1d93c0ed96cfa8ea23e8f69e30a220f6
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-03 14:35:04 -0700

    akhd

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -27,11 +27,12 @@ void print_diagsums(int *a[], int size)
 	}
 
 	sum2 = 0;
-	for (i = 0; i < size; i++)
+	size--;
+	for (i = 0; i <= size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j <= size; j++)
 		{
-			if ((i + j) == size - 1)
+			if ((i + j) == size)
 			{
 				sum2 = sum2 + a[i][j];
 			}
