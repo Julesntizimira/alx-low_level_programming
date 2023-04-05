09b983da0450871674f668dc469a6a3258f07f36
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-05 10:50:44 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,37 @@
 # include "main.h"
 
+/**
+ * is_prime_number - funct
+ * @n: input
+ * @j: input
+ * Return: 1,0
+ */
 
+int primehelp(int n, int j);
 
 int is_prime_number(int n)
 {
-	int z = 1;
-       if (n % z == 0)
-	       return(0);
-       
-       return
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (primehelp(n, 2));
+}
+
+/**
+ * primehelp - funct
+ * @n: input
+ * @j: input
+ * Return: 1,0
+ */
+int primehelp(int n, int j)
+
+{
+	if (j == n)
+		return (1);
+	if ((n % j) == 0)
+	{
+		return (0);
+	}
+	return (primehelp(n, (j + 1)));
 }
