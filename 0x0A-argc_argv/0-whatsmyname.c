f9b0def5ce57fd19a551243015cda4ba76bc749f
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-08 02:31:23 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/**
+ * main - starting point
+ * @argc: argument to main
+ * @argv: array of pointers to the string arguments
+ * Return: 0 always success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
+
