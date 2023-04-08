d9f08e89948ff8c9d553ee55d8ca34db78e36187
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-08 04:19:42 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+# include <stdlib.h>
+/**
+ * main - starting point
+ * @argc: argument to main
+ * @argv: array of pointers to the string arguments
+ * Return: 0 always success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, add;
+
+	add = 0;
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	if (argc >= 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
+	}
+	return (0);
+}
