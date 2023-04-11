4475db910b7b8983402e2c7eae48a364610cba7a
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-11 09:49:02 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,9 +17,13 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	arr = (int **)malloc(height * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *)malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+			return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
