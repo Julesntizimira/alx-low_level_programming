c6cb7f22116e8310e88fdc615e0496bb1ba89abd
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-13 05:41:06 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,19 +10,22 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char c = '0';
+	char c = '\0';
 	char *s = NULL;
 	void *ptr = NULL;
 	unsigned int i = 0;
+	unsigned int k;
+
+
+	k = nmemb * size;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
+	ptr = malloc(k);
 	if (ptr == NULL)
 		return (NULL);
-
-	s = ptr;
-	while(i < nmemb)
+	s = (char *) ptr;
+	while (i < k)
 	{
 		s[i] = c;
 		i++;
