c7346c62478df57ad53628cfc1863c2836e2e7a0
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-13 01:02:43 -0700

    1-string_nconcat.c

--- 1-string_nconcat.c
+++ 1-string_nconcat.c
@@ -1,1 +1,62 @@
-1-string_nconcat.c
+# include "main.h"
+# include <stdlib.h>
+# include <stdio.h>
+/**
+ * string_nconcat - funct
+ * @s1: input
+ * @s2: input
+ * @n: input
+ * Return: ptr
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1, len2, len3, i, j;
+	char *ptr = NULL;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	if (n >= len2)
+	{
+		n = len2;
+	}
+
+	len3 = len1 + n + 1;
+	ptr = malloc(len3 * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		ptr[len1 + j] = s2[j];
+	}
+	ptr[len3] = '\0';
+
+	return (ptr);
+
+}
+/**
+ * _strlen - funct
+ * @s: input
+ * Return: int
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int len = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
