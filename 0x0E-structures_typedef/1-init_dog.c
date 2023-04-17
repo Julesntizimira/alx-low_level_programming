ab86964e74f2c4e77c73eecd970201e9d2ce5319
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-17 08:22:01 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+# include "dog.h"
+# include <stdlib.h>
+/**
+ * init_dog - funct
+ * @d: structure
+ * @name: member variable
+ * @age: member variable
+ * @owner: member variable
+ * Return: void pointer
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
