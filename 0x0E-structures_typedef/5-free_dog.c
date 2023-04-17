a53defc4db1ad16523d503576951bdbc2912f8e9
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-17 11:48:55 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,10 +7,9 @@
 
 void free_dog(dog_t *d)
 {
-	if (d->owner != NULL)
-		free(d->owner);
-	if (d->name != NULL)
-		free(d->name);
-	if (d != NULL)
-		free(d);
+	free(d->name);
+	d->name = NULL;
+	free(d->owner);
+	d->owner = NULL;
+	free(d);
 }
