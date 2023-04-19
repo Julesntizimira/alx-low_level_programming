f1da4a9fdaf1bc45d81bb77c8a2ea14ad75ae652
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-19 15:31:56 -0700

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,20 +8,19 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-        op_t ops[] = {
-                {"+", op_add},
-                {"-", op_sub},
-                {"*", op_mul},
-                {"/", op_div},
-                {"%", op_mod},
-                {NULL, NULL}
-        };
-        int i;
-
-        for (i = 0; i < 6; i++)
-        {
-                if (ops[i].op[0] == *s)
-                        return (ops[i].f);
-        }
-        return (NULL);
-}   
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+	for (i = 0; i < 6; i++)
+	{
+		if (ops[i].op[0] == *s)
+			return (ops[i].f);
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+# include "3-calc.h"
+/**
+ * op_add - addition
+ * @a: input
+ * @b: input
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - addition
+ * @a: input
+ * @b: input
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - addition
+ * @a: input
+ * @b: input
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - addition
+ * @a: input
+ * @b: input
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - addition
+ * @a: input
+ * @b: input
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
