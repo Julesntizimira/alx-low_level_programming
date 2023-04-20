5865b1adb71373e324f6f16b2366a391b8a2347b
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-19 18:27:57 -0700

    m

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - funct
+ *
+ * Return: 0 always success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int num_bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	unsigned char *main_fn_bytes = (unsigned char *)main;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", main_fn_bytes[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
+
