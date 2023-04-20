433b8c6621b2fa8ad0873bf740e5e4081905421d
Ntizimira Mutsinzi Jules <ntizimijules5@gmail.com>
2023-04-19 18:08:09 -0700

    m

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,5 @@
 # include "3-calc.h"
-# include <stddef.h>
+
 /**
  * get_op_func - funct
  * @s: input
@@ -13,13 +13,12 @@ int (*get_op_func(char *s))(int a, int b)
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+		{"%", op_mod}
 	};
 	int i;
 
 	i = 0;
-	while (i < 6)
+	while (i < 5)
 	{
 		if (ops[i].op[0] == *s)
 			return (ops[i].f);
