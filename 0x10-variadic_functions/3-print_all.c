17c62ee7f4cd946235ad20a71bb5eb15a628c89b
jules ntizimira <ntizimijules@gmail.com>
2023-04-21 05:01:12 +0000

    m

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -25,7 +25,7 @@ void print_all(const char * const format, ...)
 					printf("%s%d", v, va_arg(args, int));
 					break;
 				case 'f':
-					printf("%s%f", v, (float)(va_arg(args, double));
+					printf("%s%f", v, (float)va_arg(args, double));
 					break;
 				case 's':
 					s = va_arg(args, char *);
