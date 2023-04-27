0133e8141097fc0abd42f12434de6e7b6f6aa7b7
jules ntizimira <ntizimijules@gmail.com>
2023-04-27 14:55:53 +0000

    3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,52 @@
+# include "lists.h"
+
+/**
+ * add_node_end - function that adds node to the beginning
+ * @head: pointer to node head
+ * @str: string input
+ * Return: poiter to new add node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ptr = NULL, *p = NULL;
+
+	ptr = malloc(sizeof(list_t));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->str = strdup(str);
+	ptr->len = _strlen(str);
+
+	if (*head == NULL)
+	{
+		*head = ptr;
+		return (ptr);
+	}
+	else
+	{
+		p = *head;
+		while (p->next != NULL)
+		{
+			p = p->next;
+		}
+		p->next = ptr;
+	}
+	return (ptr);
+}
+/**
+ * _strlen - function to mesure the length
+ * @str: string pointer
+ * Return: lenght
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int len = 0;
+
+	while (*str != '\0')
+	{
+		str++;
+		len++;
+	}
+	return (len);
+}
