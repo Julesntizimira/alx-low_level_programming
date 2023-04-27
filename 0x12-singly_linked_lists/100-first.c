12e4aee6814a058ab4df3d0f21e9e27e41370f09
jules ntizimira <ntizimijules@gmail.com>
2023-04-27 15:54:29 +0000

    100-main.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+# include "lists.h"
+
+/**
+ * print_message - void fuction that be executed before main
+ */
+void __attribute__((constructor)) print_message()
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
