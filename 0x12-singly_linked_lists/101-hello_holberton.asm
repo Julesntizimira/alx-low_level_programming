37b22fc934af34c8db9e8d422eba47bbd061a20b
jules ntizimira <ntizimijules@gmail.com>
2023-04-27 16:34:05 +0000

    asmb

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,33 @@
-section .data
-    message db "Hello, Holberton", 0
-    format db "%s\n", 0
-
-section .text
-    extern printf
-    global main
-
+	.arch armv8-a
+	.file	"hello.c"
+	.text
+	.section	.rodata
+	.align	3
+.LC0:
+	.string	"Hello, Holberton"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
 main:
-    ; call printf with message and format arguments
-    mov rdi, format
-    mov rsi, message
-    xor rax, rax
-    call printf
-
-    ; exit program
-    xor eax, eax
-    ret
-
-printf:
-    ; define printf function
-    ; implementation omitted for brevity
-    ; should follow standard calling convention for printf
-    ret
+.LFB0:
+	.cfi_startproc
+	stp	x29, x30, [sp, -16]!
+	.cfi_def_cfa_offset 16
+	.cfi_offset 29, -16
+	.cfi_offset 30, -8
+	mov	x29, sp
+	adrp	x0, .LC0
+	add	x0, x0, :lo12:.LC0
+	bl	puts
+	mov	w0, 0
+	ldp	x29, x30, [sp], 16
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0"
+	.section	.note.GNU-stack,"",@progbits
