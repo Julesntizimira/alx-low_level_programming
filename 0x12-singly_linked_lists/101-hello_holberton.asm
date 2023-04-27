5a2ce864e82bb7d59d280c978c6196ed6da39ecc
jules ntizimira <ntizimijules@gmail.com>
2023-04-27 16:45:38 +0000

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,33 +1,18 @@
-	.arch armv8-a
-	.file	"hello.c"
-	.text
-	.section	.rodata
-	.align	3
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.align	2
-	.global	main
-	.type	main, %function
-main:
-.LFB0:
-	.cfi_startproc
-	stp	x29, x30, [sp, -16]!
-	.cfi_def_cfa_offset 16
-	.cfi_offset 29, -16
-	.cfi_offset 30, -8
-	mov	x29, sp
-	adrp	x0, .LC0
-	add	x0, x0, :lo12:.LC0
-	bl	puts
-	mov	w0, 0
-	ldp	x29, x30, [sp], 16
-	.cfi_restore 30
-	.cfi_restore 29
-	.cfi_def_cfa_offset 0
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0"
-	.section	.note.GNU-stack,"",@progbits
+section .data
+    hello db 'Hello, Holberton',0
+
+section .text
+    global _start
+
+_start:
+    ; write "Hello, Holberton" to stdout
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, hello
+    mov edx, 16
+    int 0x80
+
+    ; exit with status 0
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
