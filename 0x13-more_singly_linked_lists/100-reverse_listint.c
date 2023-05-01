82d306b079e9fa872216b036e34676f70f01e540
jules ntizimira <ntizimijules@gmail.com>
2023-05-01 12:54:41 +0000

    m

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,7 +12,7 @@ listint_t *reverse_listint(listint_t **head)
 		return (current);
 
 	temp = current->next;
-	while (current->next)
+	while (temp != *head)
 	{
 		*head = temp;
 		if (temp->next == NULL)
