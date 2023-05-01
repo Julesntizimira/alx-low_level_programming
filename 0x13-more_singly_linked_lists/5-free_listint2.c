8330fabf7d4879584fff535bd1025ca5ea48b31b
jules ntizimira <ntizimijules@gmail.com>
2023-05-01 08:09:11 +0000

    m

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,9 +12,9 @@ void free_listint2(listint_t **head)
 
 	while (*head)
 	{
-		ptr = *head;
-		*head = (*head)->next;
-		free(ptr);
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
 	}
 	*head = NULL;
 }
