b10f670205c9db5fece7a875f365f91be0df2bcf
jules ntizimira <ntizimijules@gmail.com>
2023-05-01 11:15:31 +0000

    m

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				if (current->next == NULL)
 				{
 					if (count == idx - 1)
+					{
 						current->next = new;
+						return (new);
+					}
 					else
 						return (NULL);
 				}
