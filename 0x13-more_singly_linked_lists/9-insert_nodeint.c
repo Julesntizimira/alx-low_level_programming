728c9dd2bca0f5cf2d1ad32195d132c7b1a298c7
jules ntizimira <ntizimijules@gmail.com>
2023-05-01 10:58:07 +0000

    m

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			*head = new;
 		else
 		{
-			for (count = 0; count < idx; count++)
+			for (count = 1; count < idx; count++)
 			{
 				temp = current;
 				if (current->next == NULL)
