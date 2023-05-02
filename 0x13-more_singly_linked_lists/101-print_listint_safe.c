b436caea85bf4d2e6cb9944c6be037c7748b86c2
jules ntizimira <ntizimijules@gmail.com>
2023-05-02 10:01:29 +0000

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,6 +10,8 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *temp = NULL, *nextnode = NULL;
 	size_t count = 0;
 
+	if (head == NULL)
+		exit(98);
 	temp = head;
 	nextnode = temp;
 
