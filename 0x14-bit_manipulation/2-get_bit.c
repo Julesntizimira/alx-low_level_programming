8c2efb90566db38c38a35d364cc02930635455fe
jules ntizimira <ntizimijules@gmail.com>
2023-05-04 13:07:04 +0000

    m

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,27 +1,25 @@
 # include "main.h"
 
 /**
- * get_bit - function to return buffer at certain index
+ * get_bit - function to return bit at certain index
  * @n: input Uint
  * @index: input
  * Return: bit at certain position
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int idx = 0, i = 0, *ptr = NULL;
+	unsigned int idx = 0, i = 0, b = 0;
 
-	ptr = malloc((index + 1) * sizeof(unsigned int));
-	if (ptr == NULL)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	do {
 		i = n / 2;
-		ptr[idx] = n % 2;
+		b = n % 2;
 		n = i;
 		if (idx == index)
-			return (ptr[idx]);
+			return (b);
 		idx++;
 	} while (n > 0);
 
 	return (-1);
-
 }
