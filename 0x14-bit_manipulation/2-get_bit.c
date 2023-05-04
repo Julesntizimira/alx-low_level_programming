e04df7f6e899a1d9dc663a33a54b3769ea172ec5
jules ntizimira <ntizimijules@gmail.com>
2023-05-04 12:46:32 +0000

    p

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,17 +8,17 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int idx = 0, i = 0;
-	int b = 0;
+	unsigned int idx = 0, i = 0, *ptr = NULL;
 
-	if (index == 0)
-		return (0);
+	ptr = malloc((index + 1) * sizeof(unsigned int));
+	if (ptr == NULL)
+		return (-1);
 	do {
 		i = n / 2;
-		b = n % 2;
+		ptr[idx] = n % 2;
 		n = i;
 		if (idx == index)
-			return (b);
+			return (ptr[idx]);
 		idx++;
 	} while (n > 0);
 
