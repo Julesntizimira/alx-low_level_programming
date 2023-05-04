d06cf0b6c4620e800789aa57ea6ba05fffeb4916
jules ntizimira <ntizimijules@gmail.com>
2023-05-04 13:13:44 +0000

    m

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,18 +8,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int idx = 0, i = 0, b = 0;
-
-	if (index >= sizeof(unsigned long int) * 8)
+	int v = 0;
+	
+	if (index > 63)
 		return (-1);
-	do {
-		i = n / 2;
-		b = n % 2;
-		n = i;
-		if (idx == index)
-			return (b);
-		idx++;
-	} while (n > 0 && idx <= index);
-
-	return (-1);
+	
+	v = (n >> index) & 1;
+	return (v);
 }
