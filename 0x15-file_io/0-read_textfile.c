cf11782128b7ea5a20b9f6bf4c733e06a8ba04ce
jules ntizimira <ntizimijules@gmail.com>
2023-05-08 11:35:56 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,18 +15,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
+	if (letters == 0)
+		return (1);
 	ptr = malloc(letters * sizeof(char));
 	if (ptr == NULL)
 		return (0);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	i = read(fd, ptr, letters);
+	if (i == -1)
+		return (0);
 	j = write(STDOUT_FILENO, ptr, letters);
+	if (j == -1)
+		return (0);
 	close(fd);
 	free(ptr);
-	if (i == -1 || j == -1)
-		return (0);
 	k = letters;
 	if (i != k || j != k)
 		return (0);
