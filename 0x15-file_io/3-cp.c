49a54fb12e5b759e997cf2f1b31569d3d58cb113
jules ntizimira <ntizimijules@gmail.com>
2023-05-08 18:23:37 +0000

    m

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,13 +23,13 @@ int main(int argc, char *argv[])
 	z = read(fr, buff, 1024);
 
 	do {
-		if (fr == -1 || z < 0)
+		if (fr == -1 || z == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			exit(98);
 		}
-		k = dprintf(fc, "%s", buff);
-		if (fc == -1 || k < 0)
+		k = write(fc, buff, z);
+		if (fc == -1 || k == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99);
@@ -38,13 +38,13 @@ int main(int argc, char *argv[])
 	i = close(fr);
 	if (i == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fr);
 		exit(100);
 	}
 	j = close(fc);
 	if (j == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", j);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fc);
 		exit(100);
 	}
 	return (0);
