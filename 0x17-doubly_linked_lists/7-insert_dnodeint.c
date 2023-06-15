2ddb8b187242a8362425f65b0eb31a2416a7004d
jules ntizimira <ntizimijules@gmail.com>
2023-06-15 12:01:51 +0000

    7th task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = NULL, *newnode = NULL;
 	unsigned int count = 0;
-	
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	tmp = *h;
@@ -22,21 +22,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (newnode == NULL)
 				return (NULL);
 			newnode->n = n;
-			newnode->prev = tmp;
-			if (tmp->next != NULL)
-			{
-				newnode->next = tmp->next;
-				tmp->next->prev = newnode;
-			}
-			tmp->next = newnode;
+
+			newnode->prev = tmp->prev;
+			tmp->prev->next = newnode;
+			newnode->next = tmp;
+			tmp->prev = newnode;
 			return (newnode);
 		}
 		count++;
 		tmp = tmp->next;
 	}
 	if (count == idx && tmp == NULL)
-	{
 		return (add_dnodeint_end(h, n));
-	}
+
 	return (NULL);
 }
